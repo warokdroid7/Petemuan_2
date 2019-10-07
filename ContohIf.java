2843ec234f14b1bba3afd533f659ace0721c3e3b
Erik Rio Setiawan <42199285+erikrios@users.noreply.github.com>
2019-10-07 12:27:44 +0700

    Add ContohFile.java

---
+++ ContohIf.java
@@ -0,0 +1,9 @@
+public class ContohIf {
+
+    public static void main(String[] args) {
+        int angka = 10;
+        if (angka == 20) {
+            System.out.println("Angka bernilai 20");
+        }
+    }
+}
