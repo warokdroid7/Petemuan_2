46927af525ae0f1a1e383adb3119e92f9ddffab5
warokdroid7 <56916572+warokdroid7@users.noreply.github.com>
2019-10-24 03:45:42 +0700

    Tambah file Shinobi.java

---
+++ Shinobi.java
@@ -0,0 +1,41 @@
+public class Shinobi
+{
+	private String shinobiName;
+	private String villageFrom;
+	private double speed;
+	private int power;
+
+	public void setShinobiName(String shinobiName){
+		this.shinobiName = shinobiName;
+	}
+	
+	public String getShinobiName(){
+		return shinobiName;
+	}
+	
+	public void setVillageFrom(String villageFrom){
+		this.villageFrom = villageFrom;
+	}
+	
+	public String getVillageFrom(){
+		return villageFrom;
+	}
+	
+	public void setSpeed(double speed){
+		this.speed = speed;
+	}
+	
+	public double getSpeed(){
+		return speed;
+	}
+	
+	public void setPower(int power){
+		this.power = power;
+	}
+	
+	public int getPower(){
+		return power;
+	}
+	
+	
+}
